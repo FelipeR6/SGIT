e9e71dbcb1ac5656a67cc932c13793c5c537ad29
Felipe Riaño <143687093+FelipeR6@users.noreply.github.com>
2025-04-19 22:26:23 -0500

    Aplicativo Web
    100% tanto conexión, como vistas y crud

---
+++ api/ubicacion.php
@@ -0,0 +1,42 @@
+<?php
+// Permite el acceso desde cualquier origen
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+
+// Establece los parámetros de conexión
+$host = "localhost";
+$username = "root";
+$password = "";
+$database = "sgit";
+
+// Conexión a la base de datos
+$conn = new mysqli($host, $username, $password, $database);
+
+// Verifica si hubo un error de conexión
+if ($conn->connect_error) {
+    die(json_encode(["error" => "Conexión fallida: " . $conn->connect_error]));
+}
+
+// Consulta SQL para obtener los equipos y sus datos
+$sql = "SELECT * FROM ubicacion";
+
+// Ejecuta la consulta SQL
+$result = $conn->query($sql);
+
+// Verifica si la consulta devuelve resultados
+if ($result->num_rows > 0) {
+    // Almacena los resultados en un array
+    $consulta = [];
+    while ($row = $result->fetch_assoc()) {
+        $consulta[] = $row;
+    }
+    // Devuelve los datos en formato JSON
+    echo json_encode($consulta);
+} else {
+    // Si no hay resultados, devuelve un mensaje de error
+    echo json_encode(["error" => "No se encontraron equipos"]);
+}
+
+// Cierra la conexión a la base de datos
+$conn->close();
+?>
